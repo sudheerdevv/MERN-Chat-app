b792d512202bf41dabd8500c2985eeca6a7f1b55
Sudheer5V <sudheerbannu9@gmail.com>
2023-06-14 11:28:43 +0530

    refined commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,24 @@ import Pusher from "pusher-js";
 import { useEffect, useState } from "react";
 import "./styles/App.css";
 import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 import axios from "./axios";
-import Login from "./components/Login";
 import { TfiSearch } from "react-icons/tfi";
 import { AiOutlineUserAdd } from "react-icons/ai";
 import { FaUserCheck } from "react-icons/fa";
-import { Link, Route, Routes } from "react-router-dom";
-import Chat from "./components/Chat";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import { apiAuth } from "./config";
 import { IoChatbubblesOutline, IoCloseCircleOutline } from "react-icons/io5";
 import { CiUser } from "react-icons/ci";
-import { useNavigate } from "react-router-dom";
+import Login from "./components/Login";
+import Chat from "./components/Chat";
 
-function App() {
-  const [state, setState] = useState(false);
+const App = () => {
+  const [user] = useAuthState(auth);
   const [users, setUsers] = useState([]);
-  const [search, setSearch] = useState();
   const [filteredusers, setFilteredusers] = useState([]);
   const [addedusers, setAddedusers] = useState([]);
   const [sidebarstate, setSidebarstate] = useState(false);
-
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -41,21 +39,23 @@ function App() {
   }, [addedusers]);
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setState(!state);
+    if (user) {
+      axios.get("/getactiveusers", apiAuth).then((users) => {
+        setUsers(users.data);
+      });
 
-        axios
-          .get(`/getnewusers?useremail=${auth.currentUser?.email}`, apiAuth)
-          .then((users) => setAddedusers(users.data));
+      axios
+        .get(`/getnewusers?useremail=${auth.currentUser?.email}`, apiAuth)
+        .then((users) => setAddedusers(users.data));
 
-        axios
+      (async () => {
+        await axios
           .post(
             "/activeuser",
             {
-              name: user?.displayName,
-              email: user?.email,
-              imgUrl: user?.photoURL,
+              name: auth.currentUser?.displayName,
+              email: auth.currentUser?.email,
+              imgUrl: auth.currentUser?.photoURL,
               timestamp:
                 new Date().toLocaleDateString() +
                 " " +
@@ -69,27 +69,13 @@ function App() {
             }
           )
           .then(() => {
-            console.log("Success");
+            console.log("SUCCESS");
           });
-      } else {
-        setState(false);
-      }
-    });
-
-    axios.get("/getactiveusers", apiAuth).then((users) => {
-      setUsers(users.data);
-    });
-
-    return unsubscribe;
-  }, []);
-
-  useEffect(() => {
-    setFilteredusers(users.filter((item) => item.email.includes(search)));
-
-    if (search === "") {
-      setFilteredusers([]);
+      })();
+    } else {
+      console.log("NO USER");
     }
-  }, [users, search]);
+  }, [user]);
 
   const addUser = (e, item) => {
     axios
@@ -107,135 +93,145 @@ function App() {
       .catch((err) => console.log(err.message));
   };
 
-  if (!state) {
-    return <Login />;
-  } else
-    return (
-      <div className="app">
-        <div className={`side_bar ${sidebarstate ? "sidebar_open" : ""}`}>
-          <div className="search_bar">
-            <div className="search_input_bar">
-              <input
-                className="search_input"
-                type="text"
-                onChange={(e) => setSearch(e.target.value)}
-                placeholder="Search"
-              />
-              <TfiSearch />
-            </div>
-
-            <div className="add_userscontainer">
-              {filteredusers.map((item) => {
-                const existState = addedusers.find(
-                  (data) => data.chatuseremail === item.email
+  if (!user) return <Login />;
+  return (
+    <div className="app">
+      <div className={`side_bar ${sidebarstate ? "sidebar_open" : ""}`}>
+        <div className="search_bar">
+          <div className="search_input_bar">
+            <input
+              className="search_input"
+              type="text"
+              onChange={(e) => {
+                console.log("seee");
+                setFilteredusers(
+                  users.filter((item) =>
+                    item.email.includes(e.target.value.toLowerCase())
+                  )
                 );
 
-                return (
-                  <div className="profile_container">
-                    <img src={item.imgUrl} className="profile_image" alt="" />
-                    <div className="profile_info">
-                      <p className="profile_title">{item.name}</p>
-                      <p className="profile_email">{item.email}</p>
-                    </div>
-                    {auth.currentUser.email === item.email ? (
-                      <h5 style={{ margin: "0 10px", textAlign: "center" }}>
-                        You
-                      </h5>
-                    ) : (
-                      <button
-                        disabled={existState === undefined ? false : true}
-                        className="profile_button"
-                        onClick={(e) => addUser(e, item)}
-                      >
-                        {existState === undefined ? (
-                          <AiOutlineUserAdd size={17} />
-                        ) : (
-                          <FaUserCheck size={17} />
-                        )}
-                      </button>
-                    )}
-                  </div>
-                );
-              })}
-            </div>
+                if (e.target.value === "") {
+                  setFilteredusers([]);
+                }
+              }}
+              placeholder="Search"
+            />
+            <TfiSearch />
           </div>
-          <div className="added_users">
-            <h3 style={{ margin: "1rem 1rem" }}>Chats</h3>
-            {addedusers.map((user) => {
-              const bufferName = btoa(user.chatusername);
-              const bufferEmail = btoa(user.chatuseremail);
+
+          <div className="add_userscontainer">
+            {filteredusers.map((item) => {
+              const existState = addedusers.find(
+                (data) => data.chatuseremail === item.email
+              );
 
               return (
-                <Link
-                  style={{ textDecoration: "none" }}
-                  to={`/rooms/${bufferEmail}/${bufferName}`}
-                  onClick={() => setSidebarstate(false)}
-                >
-                  <div className="profile_container">
-                    <img
-                      src={user.chatuserimgUrl}
-                      className="profile_image"
-                      alt=""
-                    />
-                    <div className="profile_info">
-                      <p className="profile_title">{user.chatusername}</p>
-                      <p className="profile_email">{user.chatuseremail}</p>
-                    </div>
+                <div className="profile_container">
+                  <img src={item.imgUrl} className="profile_image" alt="" />
+                  <div className="profile_info">
+                    <p className="profile_title">{item.name}</p>
+                    <p className="profile_email">{item.email}</p>
                   </div>
-                </Link>
+                  {auth.currentUser.email === item.email ? (
+                    <h5 style={{ margin: "0 10px", textAlign: "center" }}>
+                      You
+                    </h5>
+                  ) : (
+                    <button
+                      disabled={existState === undefined ? false : true}
+                      className="profile_button"
+                      onClick={(e) => addUser(e, item)}
+                    >
+                      {existState === undefined ? (
+                        <AiOutlineUserAdd size={17} />
+                      ) : (
+                        <FaUserCheck size={17} />
+                      )}
+                    </button>
+                  )}
+                </div>
               );
             })}
           </div>
+        </div>
+        <div className="added_users">
+          <h3 style={{ margin: "1rem 1rem" }}>Chats</h3>
+          {addedusers.map((user) => {
+            const bufferName = btoa(user.chatusername);
+            const bufferEmail = btoa(user.chatuseremail);
 
-          {sidebarstate ? (
-            <div
-              onClick={() => setSidebarstate(false)}
-              style={{
-                position: "absolute",
-                bottom: "10%",
-                left: "45%",
-                display: "flex",
-                justifyContent: "center",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <IoCloseCircleOutline color="white" size={30} />
-              Close
-            </div>
-          ) : (
-            ""
-          )}
+            return (
+              <Link
+                style={{ textDecoration: "none" }}
+                to={`/rooms/${bufferEmail}/${bufferName}`}
+                onClick={() => setSidebarstate(false)}
+              >
+                <div className="profile_container">
+                  <img
+                    src={user.chatuserimgUrl}
+                    className="profile_image"
+                    alt=""
+                  />
+                  <div className="profile_info">
+                    <p className="profile_title">{user.chatusername}</p>
+                    <p className="profile_email">{user.chatuseremail}</p>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
         </div>
 
-        <div className="main_chatcontainer">
-          <div className="current_profile">
-            <IoChatbubblesOutline
-              size={17}
-              className="chats_bubble"
-              onClick={() => setSidebarstate(true)}
-            />
-            <h5>
-              <span>
-                <CiUser size={20} />
-              </span>
-              {auth.currentUser?.displayName}
-            </h5>
-            <button
-              onClick={() => {
-                auth.signOut();
-                navigate("/", {});
-              }}
-            >
-              Sign out
-            </button>
+        {sidebarstate ? (
+          <div
+            onClick={() => setSidebarstate(false)}
+            style={{
+              position: "absolute",
+              bottom: "10%",
+              left: "45%",
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <IoCloseCircleOutline color="white" size={30} />
+            Close
           </div>
-          <Routes>
-            <Route path="/rooms/:room/:id" Component={Chat} />
-          </Routes>
-        </div>
+        ) : (
+          ""
+        )}
       </div>
-    );
-}
+
+      <div className="main_chatcontainer">
+        <div className="current_profile">
+          <IoChatbubblesOutline
+            size={17}
+            className="chats_bubble"
+            onClick={() => setSidebarstate(true)}
+          />
+          <h5>
+            <span>
+              <CiUser size={20} />
+            </span>
+            {auth.currentUser?.displayName}
+          </h5>
+          <button
+            onClick={() => {
+              auth.signOut();
+              navigate("/", {});
+            }}
+          >
+            Sign out
+          </button>
+        </div>
+
+        <Routes>
+          <Route path="/rooms/:room/:id" Component={Chat} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9000/v1/app",
+  baseURL: "http://localhost:9000/v1/app/",
 });
 
 export default instance;
